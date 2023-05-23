ac509eb5a5abc5670a8003cae435c4069f581092
soufiane <soufianetaoufikofficial@gmail.com>
2023-05-23 18:37:38 +0100

    move container style to external stylesheet

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,6 +7,7 @@ import {
   TopNavigation,
   TopNavigationAction,
 } from '@ui-kitten/components';
+import styles from './HomeScreenStyles';
 
 const AddIcon = props => <Icon {...props} name="plus-outline" />;
 
@@ -20,7 +21,7 @@ export const HomeScreen = ({navigation}) => {
   );
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.container}>
       <TopNavigation
         title="Home Screen"
         alignment="center"
